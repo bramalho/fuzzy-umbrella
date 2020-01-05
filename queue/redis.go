a71c0f3ff1a653eca3eef68f1cc4dc10812bd8b3
Bruno Ramalho <bfmramalho@gmail.com>
2020-01-05 21:34:20 +0100

    redis queue

---
+++ queue/redis.go
@@ -0,0 +1,41 @@
+package queue
+
+import (
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/adeven/redismq"
+	"github.com/joho/godotenv"
+)
+
+var queue *redismq.Queue
+var consumer *redismq.Consumer
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	redisHost := os.Getenv("REDIS_HOST")
+	redisPort := os.Getenv("REDIS_PORT")
+	redisPass := os.Getenv("REDIS_PASS")
+	redisDB, _ := strconv.ParseInt(os.Getenv("REDIS_DB"), 10, 64)
+	redisQueue := os.Getenv("REDIS_QUEUE")
+
+	queue := redismq.CreateQueue(redisHost, redisPort, redisPass, redisDB, redisQueue)
+
+	consumer, err := queue.AddConsumer("my_consumer")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func GetQueue() {
+	return queue
+}
+
+func GetConsumer() {
+	return consumer
+}
